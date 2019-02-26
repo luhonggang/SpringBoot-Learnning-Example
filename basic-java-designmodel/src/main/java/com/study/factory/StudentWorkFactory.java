c201fec93e80a879f8e3e75358c857144daa4a47
luhonggang <XIAOniu666>
2019-02-26 17:36:48 +0800

    设计模式项目提交代码注释

--- basic-java-designmodel/src/main/java/com/study/factory/StudentWorkFactory.java
+++ basic-java-designmodel/src/main/java/com/study/factory/StudentWorkFactory.java
@@ -4,6 +4,7 @@ package com.study.factory;
  * @author luhonggang
  * @version 1.8.0
  * @date 2019/2/26 17:03
+ * 定义的工厂方法返回相应的Work类子类的 实例(此处 是返回StudentWork实例)
  */
 public class StudentWorkFactory implements IworkFactory {
     @Override
--- basic-java-designmodel/src/main/java/com/study/factory/TeacherWorkFactory.java
+++ basic-java-designmodel/src/main/java/com/study/factory/TeacherWorkFactory.java
@@ -4,6 +4,7 @@ package com.study.factory;
  * @author luhonggang
  * @version 1.8.0
  * @date 2019/2/26 16:59
+ * 定义的工厂方法返回相应的Work类子类的 实例(此处 是返回 TeacherWork 实例)
  */
 public class TeacherWorkFactory implements IworkFactory {
     /**
