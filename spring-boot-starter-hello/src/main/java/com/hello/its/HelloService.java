decd4f2f267f7d9f6623265f0dff5c88b4af9542
luhonggang <XIAOniu666>
2019-07-11 16:24:33 +0800

    基于springboot配置扫描实现自动注入工程外部的bean到spring容器

--- spring-boot-starter-hello/src/main/java/com/hello/its/HelloService.java
+++ spring-boot-starter-hello/src/main/java/com/hello/its/HelloService.java
@@ -10,8 +10,21 @@ public class HelloService {
 
     public static final String TIME = "2019-07-11";
 
+    private String msg;
+
     public void sayHello(String msg){
         System.out.println("此时此刻你好意思对我说: " + msg);
     }
 
+    public void sayHello(){
+        System.out.println("默认值输出 : " + msg);
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
